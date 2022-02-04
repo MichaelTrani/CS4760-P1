4a870be5060e33da9c757b6704aa60f66c5a39f3
Michael Trani <mftc64@umsystem.edu>
2022-02-03 22:00:56 -0800

    Proper chain re-written with output file handling.

--- p1/chain.cpp
+++ p1/chain.cpp
@@ -1,6 +1,13 @@
-#include"chain.h"
+/*
+Written by Michael Trani Jan 2022
+CMP SCI 4760 Operating Systems
+This program creates a chain of children processes and logs their PIDs.
 
+*/
+
+#include"chain.h"
 using namespace std;
+
 int main(int argc, char* argv[]){
 
     string programName = argv[0];
@@ -11,13 +18,11 @@ int main(int argc, char* argv[]){
     int nchars = 80; //number of characters read into the buffer
     int sleeptime = 3; //time to sleep in each iteration
     int niters = 1;     //number of iterations in the loop :double check for default
-    //string textfile = "file";  THIS MIGHT BE HANDLED BY THE KERNEL
+    string outfile = "file";  //THIS MIGHT BE HANDLED BY THE KERNEL
 
+    ifstream helper(".help");
 
-
-    ifstream helper("help");
-
-    while ((option = getopt(argc, argv, "hp:c:s:i:")) != -1) {
+    while ((option = getopt(argc, argv, "hp:c:s:i:f:")) != -1) {
         switch (option)
         {
         case 'h':
@@ -28,6 +33,10 @@ int main(int argc, char* argv[]){
 
         case 'p':
             nprocs = atoi(optarg);
+            if (nprocs > 100 || nprocs < 1){
+                cout << "Invalid process count.\n";
+                return 1;
+            }
             break;
 
         case 'c':
@@ -42,13 +51,22 @@ int main(int argc, char* argv[]){
             niters = atoi(optarg);
             break;
 
+        case 'f':
+            outfile = optarg;
+            break;
+
         default:
             break;
         }
     }
 
     helper.close();
-    cout << programName << endl;
+    if ((pFile = fopen(outfile.c_str(), "w")))
+        ;
+    else
+        perror("Chain: Output File Error");
+
+    //cout << programName << endl;
     processor(nprocs, sleeptime);
 
 
--- p1/chain.h
+++ p1/chain.h
@@ -6,26 +6,107 @@
 #include <iostream>
 #include <unistd.h>
 #include <fstream>
-//#include "processes.h"
+#include <sys/wait.h>
 
-//#include <cstring>
-//#include <stdio.h>
-//#include <stdlib.h>
+FILE* pFile;
+
 
 void processor(int n, int sleepIn) {
+
 	pid_t childpid = 0;
 	int i;
 	for (i = 1; i < n; i++)
-		if ((childpid = fork()))
+		if (childpid = fork()){
+			if(childpid < 0){
+				perror("Chain: Error forking child");
+				return;
+			}
 			break;
+		}
+
+	fprintf(pFile, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
+		i, (long)getpid(), (long)getppid(), (long)childpid);
 	fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
 		i, (long)getpid(), (long)getppid(), (long)childpid);
-	sleep(sleepIn);
+
+
+	return;
+
 }
 
 
+
+	//pid_t childpid = 0;
+	//pid_t mypid;
+	//pid_t parentpid;
+	//pid_t waitreturn;
+	//int i;
+	//for (i = 1; i < n; i++) {
+	//	mypid = getpid();
+	//	parentpid = getppid();
+
+	//	if ((childpid = fork()) < 0)
+	//	{
+	//		perror("Error forking child");
+	//		//return (1);			// Terminate code
+	//	}
+	//	else
+	//		break;
+	//}
+
+	//while (childpid != (waitreturn = wait(NULL)))
+	//	if ((waitreturn == -1) && (errno != EINTR))
+	//		break;
+
+
+	//fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
+	//		i, mypid, parentpid, childpid);
+
+	//sleep(sleepIn);
+
+	//fprintf(stderr, "i:%i my ID:%ld parent ID:%ld\n",
+	//	i,(long)getpid(), (long)getppid() );
 
 
 
 
 #endif
+
+//
+//pid_t childpid, cpid;		// Process id for child
+//char command[] = "ls";		// Command to be executed
+//char path[] = "/bin/ls";		// Path for the command
+//int status;				// Return value from child
+//
+//// Fork child
+//
+//if ((childpid = fork()) < 0)
+//{
+//	perror("Error forking child");
+//	return (1);			// Terminate code
+//}
+//
+//if (childpid)			// Code for parent
+//{
+//	printf("In parent\n");
+//	printf("Process id %ld Parent id %ld\n", (long)getpid(), (long)getppid());
+//	printf("Waiting for child to terminate ...\n");
+//
+//	cpid = wait(&status);
+//
+//	printf("Child %ld returned %d\n", cpid, WEXITSTATUS(status));
+//
+//	return (0);			// Parent terminates
+//}
+//
+//// This is the code for child
+//
+//sleep(10);		// Wait for 10 sec before executing child
+//execl(path, command, (char)(NULL));
+//
+//// We come to this point only if execl failed.
+//
+//perror("Problem with execl: ");
+//
+//return (1);
+//}
